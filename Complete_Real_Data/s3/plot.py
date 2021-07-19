09691e9a172333b1e7611e1f784b9a6389d3a107
yemaedahrav <ameyvarhade@gmail.com>
2021-07-19 18:17:48 +0530

    Eigenvalue Plots
    Added the eigenvalue plots for all files at all timestamps

---
+++ Complete_Real_Data/s3/plot.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+N_t = 55
+script_dir = os.path.dirname(__file__)
+results_dir = os.path.join(script_dir, 'adjusted/')
+
+for time_stamp in range(N_t):
+    print(time_stamp)
+    eig = np.loadtxt(str(time_stamp)+".txt", dtype=np.float64)
+    eig = np.asarray(eig)
+    eig[0] = 0
+
+    f = plt.figure()
+    f.set_figwidth(20)
+    f.set_figheight(7)
+
+    plt.plot(eig,color='teal', marker='*', linestyle='dotted',linewidth=0.8, markersize=3)
+    plt.xlabel('Wavenumebr m at time t={}'.format(time_stamp))
+    plt.ylabel('Eigenvalue')
+    file_name = str(time_stamp)+"_adjusted.png"
+    plt.savefig(file_name, dpi=200)
+    plt.savefig(results_dir + file_name)
---
+++ Complete_Real_Data/s3/plot_log.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+N_t = 55
+script_dir = os.path.dirname(__file__)
+results_dir = os.path.join(script_dir, 'logarithmic/')
+
+for time_stamp in range(N_t):
+    print(time_stamp)
+    eig = np.loadtxt(str(time_stamp)+".txt", dtype=np.float64)
+    eig = np.asarray(eig)
+
+    f = plt.figure()
+    f.set_figwidth(20)
+    f.set_figheight(7)
+
+    plt.plot(eig,color='teal', marker='*', linestyle='dotted',linewidth=0.8, markersize=3)
+    plt.xlabel('Wavenumebr m at time t={}'.format(time_stamp))
+    plt.ylabel('Eigenvalue')
+    plt.yscale('log')
+    file_name = str(time_stamp)+"_log.png"
+    plt.savefig(results_dir + file_name, dpi=200)
